c702883495a05bd4fa0ea9b11cc9c18971620c67
Zeenat Parween <111422656+zeenatparween@users.noreply.github.com>
2023-08-18 23:33:30 +0530

    Add files via upload

---
+++ JS Own Basic/HTML/27 Feb Assignment/Button Events/page2/Index2.js
@@ -0,0 +1,21 @@
+ let video = document.getElementById("video")
+let heading = document.getElementById("heading")
+ let para = document.getElementById("para")
+ let button = document.getElementById("btn")
+ let left = document.getElementById("left")
+
+ button.addEventListener('click', Effects)
+  function Effects(){
+     heading.style.display = "none"
+     para.style.display = "none"
+     left.style.width = "1200px"
+     left.style.marginLeft = "200px"
+     btn.style.margin = "350px 150px 0px 0px"
+     video.style.autoplay = "disable"
+
+     
+
+  
+
+}
+
